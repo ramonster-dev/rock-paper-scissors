0e82ed034ef34e7b2b712c3d2386399512c05626
ramon <rtovar.develops@gmail.com>
2022-03-26 18:19:23 +1100

    Results update with each win and loss

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
 const choices = ['sword', 'bow', 'magic'];
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score');
+const result = document.getElementsByClassName('result')[0];
 const userChoices = document.querySelectorAll('.user-choice');
 userChoices.forEach(userChoice => userChoice.addEventListener('click', playGame));
 
 function playGame() {
-  const userChoice = this.textContent;
+  const userChoice = this.textContent.toLowerCase();
   const computerChoice = getComputerChoice();
   const winner = selectWinner(userChoice, computerChoice);
-  console.log(`The winner is ${winner}`);
+  result.textContent = `The winner is ${winner}`;
 }
 
 function getComputerChoice() {
@@ -23,8 +26,10 @@ function selectWinner(userChoice, computerChoice) {
   }
 
   if (mod(x - y, choices.length) < choices.length / 2) {
+    userScore.textContent++;
     return userChoice;
   } else {
+    computerScore.textContent++;
     return computerChoice;
   }
 }
@@ -32,4 +37,15 @@ function selectWinner(userChoice, computerChoice) {
 function mod(a, b) {
   const c = a % b;
   return c < 0 ? c + b : c;
+}
+
+function displayWinner(userChoice, computerChoice, winner) {
+  console.log(`User choice: ${userChoice} : Computer choice: ${computerChoice}`);
+  if (winner == null) {
+    console.log('No one wins! The battle was a tie!');
+  } else if (userChoice === winner) {
+    console.log('You have bested this beast this time... Will you survive next time?');
+  } else {
+    console.log('You have been defeated and your inventory has been ransacked');
+  }
 }
