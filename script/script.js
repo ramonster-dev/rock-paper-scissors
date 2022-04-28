1d0aabad212f57fa0200ae870e46e3be572612c8
ramon <rtovar.develops@gmail.com>
2022-04-28 12:43:00 +1000

    Added button animations to game page

--- script/script.js
+++ script/script.js
@@ -1,71 +1,41 @@
 //-----------------------------Home--------------------------------------------
 //Home screen audio
-$(document).ready(function() {
-  $('#home').get(0).play();
+$(document).ready(function () {
+  $("#home").get(0).play();
 });
 
 //Toggle play button
-$('.play-btn a').click(function(event) {
-  const audio = document.querySelector('#click-play');
+$(".play-btn a").click(function (event) {
+  const audio = document.querySelector("#click-play");
   if (!audio) return;
 
   event.preventDefault();
-  $('#play-inactive').toggle();
-  $('#play-active').toggle();
+  $("#play-inactive").toggle();
+  $("#play-active").toggle();
   audio.currentTime = 0;
   audio.play();
+  window.location.href = "game.html";
 });
 
-
-//Game logic
-const choices = ['sword', 'bow', 'magic'];
-const userScore = document.getElementById('user-score');
-const computerScore = document.getElementById('computer-score');
-const result = document.getElementsByClassName('result')[0];
-const userChoices = document.querySelectorAll('.user-choice');
-userChoices.forEach(userChoice => userChoice.addEventListener('click', playGame));
+//---------------------------GAME---------------------------------------------
+//Adding functionality to attack tiles
+const tiles = document.querySelectorAll(".tile");
+tiles.forEach((tile) => tile.addEventListener("click", playGame));
 
 function playGame() {
-  const userChoice = this.textContent.toLowerCase();
-  const computerChoice = getComputerChoice();
-  const winner = selectWinner(userChoice, computerChoice);
-  result.textContent = `The winner is ${winner}`;
+  const userChoice = this.getAttribute("id");
+  animateBtn(userChoice);
 }
 
-function getComputerChoice() {
-  let choiceIndex = Math.floor(Math.random() * 3);
-  return choices[choiceIndex];
-}
+//Button animations
+function animateBtn(userChoice) {
+  let delayInMS = 350;
+  $(`#${userChoice}-inactive`).toggle();
+  $(`#${userChoice}-active`).toggle();
 
-function selectWinner(userChoice, computerChoice) {
-  const x = choices.indexOf(userChoice);
-  const y = choices.indexOf(computerChoice);
-
-  if (x == y) {
-    return null;
-  }
-
-  if (mod(x - y, choices.length) < choices.length / 2) {
-    userScore.textContent++;
-    return userChoice;
-  } else {
-    computerScore.textContent++;
-    return computerChoice;
-  }
-}
-
-function mod(a, b) {
-  const c = a % b;
-  return c < 0 ? c + b : c;
-}
-
-function displayWinner(userChoice, computerChoice, winner) {
-  console.log(`User choice: ${userChoice} : Computer choice: ${computerChoice}`);
-  if (winner == null) {
-    console.log('No one wins! The battle was a tie!');
-  } else if (userChoice === winner) {
-    console.log('You have bested this beast this time... Will you survive next time?');
-  } else {
-    console.log('You have been defeated and your inventory has been ransacked');
-  }
+  //Insert a delay here to make button reset to position
+  setTimeout(function () {
+    $(`#${userChoice}-active`).toggle();
+    $(`#${userChoice}-inactive`).toggle();
+  }, delayInMS);
 }
