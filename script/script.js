7d05ce8fc837aa6a0c2658893cf9e37537cd9c8a
ramon <rtovar.develops@gmail.com>
2022-05-14 13:26:03 +1000

    Added dynamic health bars and end game screen

--- script/script.js
+++ script/script.js
@@ -21,13 +21,35 @@ $(".play-btn a").click(function (event) {
 //Adding functionality to attack tiles
 const tiles = document.querySelectorAll(".tile");
 const choices = ["sword", "bow", "magic"];
+const monsterFill = document.querySelector('.healthbar_fill');
+const battleMsg = document.querySelector('.monster-choice p');
+let monsterHp = 5;
+let userHp = 5;
+const maxHp = 5;
 tiles.forEach((tile) => tile.addEventListener("click", playGame));
 
 function playGame() {
   const userChoice = this.getAttribute("id");
+  if (userChoice === 'run') {
+    let prob = Math.random();
+    if (prob > 0.7) {
+      displayMessage('run', 'run-success');
+      return;
+    } else {
+      displayMessage('run', 'run-fail');
+      renderHealth('monster');
+      checkHealth();
+      return;
+    }
+  }
+
   animateBtn(userChoice);
   const monChoice = getMonChoice();
   const winner = getWinner(userChoice, monChoice);
+
+  displayMessage(winner, userChoice, monChoice);
+  renderHealth(winner);
+  checkHealth(); //If health reaches 0, end game
 }
 
 function getMonChoice() {
@@ -35,6 +57,10 @@ function getMonChoice() {
 }
 
 function getWinner(userChoice, monChoice) {
+  if (userChoice === 'run') {
+    return 'monster';
+  }
+
   const x = choices.indexOf(userChoice);
   const y = choices.indexOf(monChoice);
 
@@ -43,15 +69,83 @@ function getWinner(userChoice, monChoice) {
   }
 
   if (mod(x - y, choices.length) < choices.length / 2) {
-    return userChoice;
+    return 'monster';
   } else {
-    return monChoice;
+    return 'user';
   }
 }
 
 function mod(a, b) {
   const c = a % b;
   return c < 0 ? c + b : c;
+}
+
+function checkHealth() {
+  if (monsterHp === 0 || userHp === 0) {
+    endGame()
+  } else {
+    return;
+  }
+}
+
+function endGame() {
+  const tileContainer = document.querySelector('.tile-container');
+  tileContainer.style.display = 'none';
+  let main = document.querySelector('main');
+  let p = document.createElement("p");
+  p.style.textAlign = 'center';
+  p.style.fontSize = 2 + 'rem';
+  let restartBtn = document.createElement("a");
+  restartBtn.setAttribute("href", "game.html"); 
+  let restartBtnImg = document.createElement("img");
+  restartBtnImg.setAttribute("src", "assets/images/btn-play.svg");
+  restartBtnImg.style.height = 4 + 'rem';
+  restartBtn.appendChild(restartBtnImg);
+
+  let restartContainer = document.createElement("div");
+  restartContainer.style.display = 'flex';
+  restartContainer.style.flexDirection = 'column';
+  restartContainer.style.justifyContent = 'center';
+  restartContainer.style.alignItems = 'center';
+  restartContainer.style.gap = 15 + 'px';
+  restartContainer.style.paddingTop = 1.5 + 'rem';
+  restartContainer.setAttribute("class", "restart");
+  restartContainer.appendChild(p);
+  restartContainer.appendChild(restartBtn);
+
+  if (userHp > 0) {
+    p.textContent = "Look like you have defeated Cakie! Would you like to play again?";
+  } else {
+    p.textContent = "Looks like you were beaten by a cake. That's kinda embarrassing... Play again?";
+  }
+
+  main.appendChild(restartContainer);
+}
+
+function displayMessage(winner, userChoice, monChoice) {
+  if (winner === 'user') {
+    battleMsg.textContent = `You have dealt a decisive blow with your ${userChoice}. Cakie regrets using ${monChoice}.`;
+  } else if (winner === 'monster') {
+    battleMsg.textContent = `Cakie cackles as it destroys your ${userChoice} with its ${monChoice}.`; 
+  } else if (winner === 'run' && userChoice === 'run-success') {
+    battleMsg.textContent = 'Looks like you managed to run away safely and have skipped this turn.';
+  } else if (winner === 'run' && userChoice === 'run-fail') {
+    battleMsg.textContent = 'Even the gluttonous cake monster is faster than you and you have failed to run away. Take some damage.';
+  } else {
+    battleMsg.textContent = `Seems like you both were overthinking and played ${userChoice}.`;
+  }
+}
+
+//Rendering healthbars
+function renderHealth(winner) {
+  if (winner === 'user') {
+    monsterHp--;
+    let percent = monsterHp / maxHp * 100;
+    monsterFill.style.width = percent + "%";
+  } else if (winner === 'monster') {
+    userHp--;
+    $(`.user-fill-${userHp}`).toggle();
+  }
 }
 
 //Button animations
